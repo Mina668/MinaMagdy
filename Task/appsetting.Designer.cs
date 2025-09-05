f91aab034c4525833d431c9a984aec635d4d2f7b
Mina Magdy William <minamagdy12321@gmail.com>
2025-09-06 01:12:12 +0300

    .

--- Task/appsetting.Designer.cs
+++ Task/appsetting.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Task.PresentationLayer {
+namespace Task6.PresentationLayer {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
